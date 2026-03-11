d74488adc106ad6e62bd7dc7eea280ba42d230ce
K-ES <your@email>
2026-03-11 05:22:16 +0200

    промежуточный для красоты

--- scripts/05 проверка количества записей.sql
+++ scripts/05 проверка количества записей.sql
@@ -1,1 +1,8 @@
-select count(1) from mart_student_lesson
+select subject_name, avg(mark) from mart_student_lesson
+group by subject_name
+order by subject_name
+
+select student_profile_id from mart_student_lesson
+order by student_profile_id
+
+delete from mart_student_lesson where student_profile_id > 100
